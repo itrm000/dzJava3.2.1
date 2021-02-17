8a964766045e49dd41794eec74e891d6a23bda14
Anatoly <itrm000@gmail.com>
2021-02-17 18:13:20 +0500

    Second commit

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -4,42 +4,92 @@ public class Post {
 
     private int id;
     private int ownerId;
+    private int fromId;
+    private int createdBy;
     private int data;
-    private TextPost text;
-    private String imageUrl;
-
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private boolean frendsOnly;
     private CommentsInfo commentsInfo;
-    private int countLike;
-    private String countViewsInfo;
-    private String countRepost;
-
-    public class TextPost {
-        private String textTextPost;
-        private String logoUrl;
-    }
+    private Copyrigth copyrigth;
+    private Report report;
+    private ViewsInfo viewsInfo;
+    private String postType;
+    private PostSourse postSourse;
+    private int signerId;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean isFavorite;
+    private int postPonedId;
+    private Donut donut;
 
     public class CommentsInfo {
-        private int idCommentsInfo;
-        private int idOwnerCommentsInfo;
-        private int dataCommentsInfo;
-        private String textCommentsInfo;
         private int countCommentsInfo;
+        private boolean canPost;
+        private boolean groupsCanPost;
+        private boolean canClose;
+        private boolean canOpen;
     }
 
+    public class Copyrigth {
+        private int idCopyrigth;
+        private String linkCopyrigth;
+        private String nameCopyrigth;
+        private String typeCopyrigth;
+    }
 
+    public class Like {
+        private int countLike;
+        private boolean canOpen;
+        private boolean canOpen;
+        private boolean canOpen;
+    }
 
+    public class Report {
+        private int countreport;
+        private boolean userReposted;
+    }
 
+    public class ViewsInfo {
+        private int countViewsInfo;
+    }
 
+    public class PostSourse {
+        private String type;
+        private String platform;
+        private String data;
+        private String url;
+    }
 
+    private class Geo {
+        private String type;
+        private String coordinates;
+        private Place place;
 
+        public class Place {
+            private int id;
+            private String titla;
+            private int latitude;
+            private int longitude;
+            private int created;
+            private String icon;
+            private int checkins;
+            private int updated;
+            private int type;
+            private int country;
+            private int city;
+            private String adress;/
+        }
+    }
 
-
-
-
-
-
-
-
-
+    public class Donut {
+        private boolean isDonut;
+        private int paidDuration;
+        private boolean canPablishFreeCopy;
+        private String editMode;
+    }
 
 }
