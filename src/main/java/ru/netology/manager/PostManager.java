68bbf85e8a2789628aae2004d01a9b13eb58b542
Anatoly <itrm000@gmail.com>
2021-02-18 01:15:12 +0500

    Third commit

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,25 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    public Post[] search(
+            int ownerId,
+            String domain,
+            String querry,
+            boolean ownersOnly,
+            int count,
+            int offset
+    ) {
+        return null;
+    }
+
+    public void delete(
+            int ownerId,
+            int postId
+    ) {
+        System.out.println("null");
+    }
+
+}
