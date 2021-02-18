8f6b59c59261bf44ec8505b364049ed6d1734ab8
Anatoly <itrm000@gmail.com>
2021-02-18 13:13:30 +0500

    Fourth commit

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -3,6 +3,13 @@ package ru.netology.manager;
 import ru.netology.domain.Post;
 
 public class PostManager {
+    private int ownerIdж;
+    private String domain;
+    private String querry;
+    private boolean ownersOnly;
+    private int count;
+    private int offset;
+    private int postId;
 
     public Post[] search(
             int ownerId,
